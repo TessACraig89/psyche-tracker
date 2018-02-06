c84a6fe10dd5e68057db1ff0129c95a92f7d37ca
TESS <tessashley.webdev@gmail.com>
2018-02-06 14:45:12 -0600

    adds Resource links

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -6,12 +6,12 @@ class Resources extends Component{
       <div>
         <h1 class="resourcesTitle">Resources</h1>
         <ul>
-          <li class="listItem1"></li>
-          <li class="listItem1"></li>
-          <li class="listItem1"></li>
-          <li class="listItem1"></li>
-          <li class="listItem1"></li>
-          <li class="listItem1"></li>
+          <li class="listItem1"><a class="listItem1Link" href="https://suicidepreventionlifeline.org/">National Suicide Prevention Lifeline</a><span class="liItem1#">1-800-273-8255</span></li>
+          <li class="listItem2"><a class="listItem2Link" href="http://www.thehotline.org/">National Domestic Violence Hotline</a><span class="listItem2#">1-800-799-7233</span></li>
+          <li class="listItem3"><a href="https://www.thehopeline.com/">The Hope Line</a><span>1-800-273-8255</span></li>
+          <li class="listItem4"><a href=""></a></li>
+          <li class="listItem5"><a href=""></a></li>
+          <li class="listItem6"><a href=""></a></li>
         </ul>
       </div>
     )
