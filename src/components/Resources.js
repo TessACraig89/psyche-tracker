d6b255bbbd52b686c70695a8c8e6867a0e82061e
TESS <tessashley.webdev@gmail.com>
2018-02-06 16:30:05 -0600

    adds more Resource links

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -9,9 +9,16 @@ class Resources extends Component{
           <li class="listItem1"><a class="listItem1Link" href="https://suicidepreventionlifeline.org/">National Suicide Prevention Lifeline</a><span class="liItem1#">1-800-273-8255</span></li>
           <li class="listItem2"><a class="listItem2Link" href="http://www.thehotline.org/">National Domestic Violence Hotline</a><span class="listItem2#">1-800-799-7233</span></li>
           <li class="listItem3"><a href="https://www.thehopeline.com/">The Hope Line</a><span>1-800-273-8255</span></li>
-          <li class="listItem4"><a href=""></a></li>
-          <li class="listItem5"><a href=""></a></li>
-          <li class="listItem6"><a href=""></a></li>
+          <li class="listItem4"><a href="https://www.translifeline.org/">Trans Lifeline</a><span>1-877-565-8860</span></li>
+          <li class="listItem5"><a href="http://www.thebody.com/index/hotlines/national.html">National AIDS Hotline</a><span>1-800-232-4636</span></li>
+          <li class="listItem6"><a href="http://addictionblog.org/support/alcohol-hotline/">Alcohol Hotline</a><span>1-888-497-6879</span></li>
+          <li class="listItem6"><a href="">Alcohol </a></li>
+          <li class="listItem6"><a href=""></a><span></span></li>
+          <li class="listItem6"><a href=""></a><span></span></li>
+          <li class="listItem6"><a href=""></a><span></span></li>
+          <li class="listItem6"><a href=""></a><span></span></li>
+          <li class="listItem6"><a href=""></a><span></span></li>
+          <li class="listItem6"><a href=""></a><span></span></li>
         </ul>
       </div>
     )
