9843937bba4510d0b4b66ba2b5fda4f1a7b304ff
TESS <tessashley.webdev@gmail.com>
2018-02-06 23:41:41 -0600

    adds font awesome

--- src/components/Log.js
+++ src/components/Log.js
@@ -1,5 +1,17 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import 'font-awesome/css/font-awesome.min.css';
 
 class Log extends Component {
-	
-export default Log
+  render() {
+    return (
+      <div>
+				<Link class="toAboutLink" to={'/about'}>CLICK HERE TO LEARN MORE ABOUT PSYCHE TRACKER</Link>
+        <h1 class="logsTitle">Logs</h1>
+				<button><i className="fa fa-plus-circle">ADD A NEW LOG</i></button>
+      </div>
+    );
+  }
+}
+
+export default Log;
--- src/components/Logs.js
+++ src/components/Logs.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
+import Log from './Log';
+import {Link} from 'react-router-dom';
 
 class Logs extends Component{
   render(){
     return (
       <div>
-        <h1>Log Page</h1>
-        <button class="addLogBtn">ADD A NEW LOG</button>
+				<Link class="toAboutLink" to={'/about'}>CLICK HERE TO LEARN MORE ABOUT PSYCHE TRACKER</Link>
+        <h1 class="logsTitle">Logs</h1>
+				<button><i className="fa fa-plus-circle">ADD A NEW LOG</i></button>
       </div>
     )
   }
