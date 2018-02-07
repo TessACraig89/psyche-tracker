2797e101506c95e5313678a7a39b8dbb79e40f5f
TESS <tessashley.webdev@gmail.com>
2018-02-07 13:27:41 -0600

    edits table structure to contain comments, rating, and emotion

--- src/components/Log.js
+++ src/components/Log.js
@@ -11,42 +11,43 @@ class Log extends Component {
         <thead>
           <tr>
             <th><button>EDIT</button></th>
-            <th></th>
-            <th></th>
+            <th>          </th>
+            <th>          </th>
             <th>10/15/2018</th>
-            <th></th>
-            <th></th>
+            <th>          </th>
+            <th>          </th>
             <th><button>DELETE</button></th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <th scope="row">1</th>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
+            <td>Exhaustion</td>
+            <td>Hunger</td>
+            <td>Anxiety</td>
+            <td>Anger</td>
+            <td>Mania</td>
+            <td>Depression</td>
+            <td>Stress</td>
           </tr>
           <tr>
-            <th scope="row">2</th>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
           </tr>
           <tr>
-            <th scope="row">3</th>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
+            <td>Comment:</td>
+            <td>Comment:</td>
+            <td>Comment:</td>
+            <td>Comment:</td>
+            <td>Comment:</td>
+            <td>Comment:</td>
+            <td>Comment:</td>
           </tr>
+
         </tbody>
       </Table>
     );
