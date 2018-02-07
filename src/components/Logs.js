c0979073b579f0a304fab796574ebc93ecb04e87
TESS <tessashley.webdev@gmail.com>
2018-02-07 00:03:39 -0600

    fixes link to / from Logs

--- src/components/Logs.js
+++ src/components/Logs.js
@@ -6,7 +6,7 @@ class Logs extends Component{
   render(){
     return (
       <div>
-				<Link class="toAboutLink" to={'/about'}>CLICK HERE TO LEARN MORE ABOUT PSYCHE TRACKER</Link>
+				<Link class="toAboutLink" to={'/'}>CLICK HERE TO LEARN MORE ABOUT PSYCHE TRACKER</Link>
         <h1 class="logsTitle">Logs</h1>
 				<button><i className="fa fa-plus-circle">ADD A NEW LOG</i></button>
       </div>
