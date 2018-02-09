67d8d118ab895522f0ec0eff17a193aa76ea311e
TESS <tessashley.webdev@gmail.com>
2018-02-09 14:33:44 -0600

    adds styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,16 +6,12 @@ class Header extends Component{
     return (
       <div className="header">
         <header>
-
             <img className="yingyangBlk" src={require("../static/images/heart&brain_black.png")}></img>
-          
           <div className="headerText">
             <h1 class="headerTitle">Psyche Tracker</h1>
             <h2 class="headerSubtitle">{"A personal logger and tracker of one's emotional and mental health"}</h2>
           </div>
-
             <img className="yingyangWht" src={require("../static/images/heart&brain_white.png")}></img>
-
         </header>
       </div>
     )
