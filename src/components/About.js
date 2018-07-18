24ea141863a00a83fa455e40987ff51086829e7b
TessACraig89 <tesscraig.webdev@gmail.com>
2018-07-18 13:02:26 -0500

    changes about the developer link to redirect to tesscraig.com

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ class About extends Component {
         <p class="aboutP">
         Psyche Tracker is for tracking and analyzing the patterns in one’s emotional, mental, and physical health in order to fuel personal growth. Users are able to create logs containing a date, ratings for various attributes, and comments. My motivation for developing Psyche Tracker was that I noticed positive results when I used a spreadsheet mirroring this concept for my own self improvement.</p><p className="aboutPar2">Lorem ipsum dolor sit amet, soleat senserit vis eu. Eu velit possit electram eam, pro id quot primis recusabo. Cu est tale salutandi, sea ex mollis impedit. Oratio eripuit ornatus et duo, usu cibo viris ne. Mel id rebum dolore euismod, ignota option splendide ea sed.Lorem ipsum dolor sit amet, soleat senserit vis eu. Eu velit possit electram eam, pro id quot primis recusabo. Cu est tale salutandi, sea ex mollis impedit. Oratio eripuit ornatus et duo, usu cibo viris ne. Mel id rebum dolore euismod, ignota option splendide.</p>
 
-        <a className="devLink" href="https://github.com/TessACraig89">LEARN MORE ABOUT THE DEVELOPER</a>
+        <a className="devLink" href="http://tesscraig.com">LEARN MORE ABOUT THE DEVELOPER</a>
       </div>
     );
   }
