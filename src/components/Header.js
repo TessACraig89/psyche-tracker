168df49fab347b8e85ccd62e4ccc385fb0161f52
TessACraig89 <tesscraig.webdev@gmail.com>
2018-07-18 19:38:01 -0500

    styles header contents using flex justify content, WIP responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,15 @@ import '../App.css';
 class Header extends Component{
   render(){
     return (
-      <div className="header">
-        <header>
+      <div className="container-full" id="header">
+        <div className="row" id="headerRow">
             <img id="yingyangBlk" src={require("../static/images/heart&brain_black.png")}></img>
           <div id="headerText">
             <h1 className="headerTitle">Psyche Tracker</h1>
             <h2 id="headerSubtitle">{"A personal logger and tracker of one's emotional and mental health"}</h2>
           </div>
             <img id="yingyangWht" src={require("../static/images/heart&brain_white.png")}></img>
-        </header>
+            </div>
       </div>
     )
   }
