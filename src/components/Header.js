5db8f7c0c3115e5b4bddb7918fabfbe19ad1d786
TessACraig89 <tesscraig.webdev@gmail.com>
2018-08-24 11:29:59 -0500

    removes header subtitle and restyles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import '../App.css';
 
+// <h2 id="headerSubtitle">{"A personal logger and tracker of one's emotional and mental health"}</h2>
 class Header extends Component{
   render(){
     return (
@@ -9,7 +10,6 @@ class Header extends Component{
             <img id="yingyangBlk" src={require("../static/images/heart&brain_black.png")}></img>
           <div id="headerText">
             <h1 className="headerTitle">Psyche Tracker</h1>
-            <h2 id="headerSubtitle">{"A personal logger and tracker of one's emotional and mental health"}</h2>
           </div>
             <img id="yingyangWht" src={require("../static/images/heart&brain_white.png")}></img>
             </div>
